41f06d9d7707dfdcff7a1a6bd8badf259cc6ffb5
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2023-04-14 20:43:12 +0530

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1 class="text-2xl font-bold">Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev" class="link link-primary">
+        kit.svelte.dev
+    </a> to read the documentation
+</p>
